28d9c44aa7c2cbac0b064a63d8cd58655d5fab95
snlamm <shmuel.lamm@gmail.com>
2016-06-03 16:07:17 -0400

    add seeds, fix models and associations, set timezone in config/application.rb to new york

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,5 +1,8 @@
 class Instructor < ApplicationRecord
-  has_many :courses
+  has_many :instructor_courses
+  has_many :courses, through: :instructor_courses
   has_many :lectures
   has_many :reviews, through: :lectures
+
+  has_secure_password
 end
