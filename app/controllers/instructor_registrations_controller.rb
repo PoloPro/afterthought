c254905215b598c64e24d1d039a0028f5ccc0b67
snlamm <shmuel.lamm@gmail.com>
2016-06-05 10:50:33 -0400

    set up student and instructor registration create actions. no action mailer yet

--- app/controllers/instructor_registrations_controller.rb
+++ app/controllers/instructor_registrations_controller.rb
@@ -4,5 +4,19 @@ class InstructorRegistrationsController < ApplicationController
   end
 
   def create
+    instructor_new = Instructor.new(student_params)
+    if instructor_new.save
+      flash[:notice] = "Success! Welcome to Afterthought!"
+      redirect_to instructor_path(instructor_new)
+    else
+      flash.now[:alert] = "Signup Failed"
+      render :new
+    end
+  end
+
+  private
+
+  def instructor_params
+    params.require(:instructor).permit(:name, :password, :password_confirmation, :email_address)
   end
 end
--- app/controllers/student_registrations_controller.rb
+++ app/controllers/student_registrations_controller.rb
@@ -4,5 +4,18 @@ class StudentRegistrationsController < ApplicationController
   end
 
   def create
+    student_new = Student.new(student_params)
+    if student_new.save
+      flash[:notice] = "Success! Welcome to Afterthought!"
+      redirect_to student_path(student_new)
+    else
+      flash.now[:alert] = "Signup Failed"
+      render :new
+    end
+  end
+
+  private
+  def student_params
+  params.require(:student).permit(:name, :password, :password_confirmation, :email_address)
   end
 end
