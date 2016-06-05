a75096f8243e072ed88f80f718f99ec09de75d52
snlamm <shmuel.lamm@gmail.com>
2016-06-05 15:04:21 -0400

    add javascript user-type toggle to signin page

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  userStart()
+  userToggle()
+})
+
+var userStart = function() {
+  if($('#session_instructor').is(':checked')) {
+    debugger
+    $("#student-signin").css("display","none")
+    $("#instructor-signin").css("display", "");
+  }else{
+    $("#instructor-signin").css("display","none")
+    $("#student-signin").css("display", "");
+  }
+}
+
+
+var userToggle = function() {
+  $("#user-toggle").change(function() {
+    if($('#session_instructor').is(':checked')) {
+      $("#student-signin").css("display","none")
+      $("#instructor-signin").css("display", "");
+    }else{
+      $("#instructor-signin").css("display","none")
+      $("#student-signin").css("display", "");
+    }
+  })
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   get '/signup', to: 'student_registrations#new'
   get '/instructor_signup', to: 'instructor_registrations#new'
+
   resources 'student_registrations', only: [:create]
   resources 'instructor_registrations', only: [:create]
-  get '/login', to: "sessons#new"
+
+  get '/login', to: "sessions#new"
+  resources 'sessions', only: [:create, :destroy]
 
   root "home#index"
-  
+
   get "instructors/home", to: "instructors#home"
   get "students/home", to: "students#home"
 end
