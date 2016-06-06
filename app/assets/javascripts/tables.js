2a4308a3ad7c004552849d76ff6d64abbe94a3e7
PoloPro <wesleybdavis@gmail.com>
2016-06-06 12:25:28 -0400

    feedback text area added

--- app/assets/javascripts/tables.js
+++ app/assets/javascripts/tables.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
+  enableTableLinks()
+})
 
+var enableTableLinks = function() {
   $('.table-row-link').on('click', function() {
     window.document.location = $(this).data('url');
   })
-
-})
+}
