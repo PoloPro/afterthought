8bb2c39509ef8b6730c9ed416a77d7d37d5faf55
PoloPro <wesleybdavis@gmail.com>
2016-06-06 11:44:39 -0400

    progress bar and jquery ui added

--- app/services/lecture_timer.rb
+++ app/services/lecture_timer.rb
@@ -1,6 +1,6 @@
 class LectureTimer
 
-  attr_reader :start_time, :lock_time, :time_remaining
+  attr_reader :start_time, :lock_time, :total_time, :time_remaining, :time_remaining_percent
 
   def initialize(lecture)
     @lecture = lecture
@@ -13,11 +13,23 @@ class LectureTimer
   def calculate_time_remaining
     @start_time = get_start_time
     @lock_time = (@start_time + 24.hour).end_of_day
-    @time_remaining = @lock_time - @start_time
+    @total_time = @lock_time - @start_time
+
+    @time_remaining = get_time_remaining
+    @time_remaining_percent = get_time_remaining_percent
   end
 
   def get_start_time
     @lecture.created_at > @lecture.finish_time ? @lecture.created_at : @lecture.finish_time
+  end
+
+  def get_time_remaining
+    time_remaining = (@lock_time - Time.now).to_i
+    time_remaining > 0 ? time_remaining : 0
+  end
+
+  def get_time_remaining_percent
+    ((@time_remaining.to_f / @total_time.to_f) * 100.0).to_i
   end
 
   def format_times
