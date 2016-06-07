fe7b82b074f3de5401c2a39eaca527ac2eef733d
snlamm <shmuel.lamm@gmail.com>
2016-06-07 09:13:19 -0400

    refactor order index views by moving to partial and add student portion of the view

--- app/services/courses_helpers.rb
+++ app/services/courses_helpers.rb
@@ -1,9 +1,9 @@
 class CoursesHelpers
 
-  def self.get_course_info(search_term)
+  def self.get_course_info(search_term, current_user)
     course = Course.find_by(title: search_term)
     course = Course.find_by(description: search_term) if course.nil?
-    CoursesHelpers.put_info_into_hash(course)
+    CoursesHelpers.put_info_into_hash(course, current_user)
   end
 
   def self.not_course_instructor?(course_id, current_user)
@@ -12,12 +12,13 @@ class CoursesHelpers
     !course_instructors.include? (current_user)
   end
 
-  def self.put_info_into_hash(course)
+  def self.put_info_into_hash(course, current_user)
     {slugified_title: course.title.parameterize,
     title: course.title,
     description: course.description,
     courseId: course.id,
     coursePath: "/courses/#{course.id}",
-    courseStudents: course.students.count}
+    courseStudents: course.students.count,
+    userClass: current_user.class.to_s}
   end
 end
