db4d6332ed55ae9960f028ca4a6a1da90d972320
PoloPro <wesleybdavis@gmail.com>
2016-06-07 19:01:14 -0400

    flash added to lecture show after emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'localhost',
+    domain:               ENV['EMAIL_HOST'],
     user_name:            ENV['EMAIL'],
     password:             ENV['EMAIL_PASSWORD'],
     authentication:       'plain',
