e0d85e715fd3b699d46fcc3de5a462674b9d5e28
snlamm <shmuel.lamm@gmail.com>
2016-06-08 12:37:44 -0400

    prettify registration email and add text.erb email templates

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -2,6 +2,9 @@ class RegistrationMailer < ApplicationMailer
 
   def registration_confirmation(user)
     @user = user
+    
+    attachments.inline['afterthought_logo.png'] = File.read('app/assets/images/afterthought_logo_darkpurple_medium.png')
+
     mail(:to => "#{user.name} <#{user.email}>", :subject => "Registration Confirmation")
   end
 
