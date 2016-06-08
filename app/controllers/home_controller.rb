0fe88e77e1227f18073f0fa7885712325abb1289
snlamm <shmuel.lamm@gmail.com>
2016-06-08 13:11:46 -0400

    change root path to a redirect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
 
+  skip_before_action :require_login, only: [:index]
+
   def index
+    if current_user.class == OpenStruct
+      redirect_to login_path
+    else
+      redirect_to courses_path
+    end
   end
 
 end
