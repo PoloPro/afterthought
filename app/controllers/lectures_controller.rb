57f516257912fcc68c57a7b1b24887be2da74526
snlamm <shmuel.lamm@gmail.com>
2016-06-07 22:25:26 -0400

    add lecture new and create functionality

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -11,10 +11,27 @@ class LecturesController < ApplicationController
     @lecture_timer = LectureTimer.new(@lecture)
   end
 
+  def new
+    @lecture = Lecture.new
+    @current_user = current_user
+    @course = Course.find_by(id: params[:course_id])
+  end
+
+  def create
+    @lecture = Lecture.new(lecture_params)
+    finish_in_minutes = params[:lecture][:finish_time].to_i.minutes
+    @lecture.finish_time = Time.now + finish_in_minutes
+    @lecture.save
+    redirect_to course_lecture_path(@lecture.course, @lecture)
+  end
+
   private
 
   def set_current_user
     @current_user ||= current_user
   end
 
+  def lecture_params
+    params.require(:lecture).permit(:title, :description, :instructor_id, :course_id)
+  end
 end
